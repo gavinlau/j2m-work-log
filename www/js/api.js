5ff2b25933097d1359fe49987135c5e8db3f87ba
gavinlau <18881935@qq.com>
2018-03-24 11:18:14 +0800

    v1.0.1

--- www/js/api.js
+++ www/js/api.js
@@ -9,6 +9,9 @@ var j2APIS = [
             "page": 1,
             "rows": 200
         },
+		"mustLocal":false,
+		"index":1,
+		"type":"cmd",
         "desc": "Get team user of the",
         "author": "redwing",
         "create-date": "2018-03-14",
@@ -26,6 +29,9 @@ var j2APIS = [
             "page": 1,
             "rows": 200
         },
+		"mustLocal":false,
+		"index":2,
+		"type":"cmd",
         "desc": "Get default calendar booking by date and userId",
         "author": "redwing",
         "create-date": "2018-03-14",
@@ -43,6 +49,9 @@ var j2APIS = [
             "endTime": 10000000000000,
             "locale": "zh_CN"
         },
+		"mustLocal":false,
+		"index":3,
+		"type":"cmd",
         "desc": "Add worklog",
         "author": "redwing",
         "create-date": "2018-03-14",
@@ -55,6 +64,9 @@ var j2APIS = [
             "p_auth": "",
             "calendarBookingId":""
         },
+		"mustLocal":false,
+		"index":4,
+		"type":"cmd",
         "desc": "delete worklog",
         "author": "redwing",
         "create-date": "2018-03-14",
@@ -67,6 +79,9 @@ var j2APIS = [
             "p_auth": "",
             "calendarBookingId":""
         },
+		"mustLocal":false,
+		"index":5,
+		"type":"cmd",
         "desc": "get worklog",
         "author": "redwing",
         "create-date": "2018-03-14",
@@ -83,7 +98,26 @@ var j2APIS = [
             "startTime": 0,
             "endTime": 10000000000000,
         },
+		"mustLocal":false,
+		"index":6,
+		"type":"cmd",
         "desc": "update worklog",
+        "author": "redwing",
+        "create-date": "2018-03-14",
+        "update-date": "2018-03-14 09:21"
+    },
+	{
+        "name": "accountLogin",
+		"url":"/j2-api/login/account_login",
+        "params": {
+            "companyId":0,
+            "account": "",
+            "password":""
+        },
+		"mustLocal":false,
+		"index":7,
+		"type":"url",
+        "desc": "account login",
         "author": "redwing",
         "create-date": "2018-03-14",
         "update-date": "2018-03-14 09:21"
@@ -96,6 +130,9 @@ var j2APIS = [
             "account": "",
             "password":""
         },
+		"mustLocal":true,
+		"index":8,
+		"type":"cmd",
         "desc": "account login",
         "author": "redwing",
         "create-date": "2018-03-14",
@@ -107,37 +144,77 @@ var j2APIS = [
 
 J2API.prototype = {
     getAPIJson: function (name) {
+	    var items=[];
         var api;
         j2APIS.forEach(function (v, i) {
-            if (v.name == name) {
-                api = v;
-            }
+			   if (v.name == name) {
+				   items.push(v);
+                }
         });
+		api=(items.length==2 && app.data.dev)?items[1]:items[0];
         return api;
     },
-    postAPI: function (name, params,callback) {
-        var apiURL="/api/jsonws/invoke"
-        apiURL="/api/jsonws/invoke/" + name + ".json";
-
-        var apiJSON=j2API.getAPIJson(name);
-        var apiParams=apiJSON.params;
+	getAPIURLAndParams:function(name,params){
+	    var urlAndParams={url:'',params:{}};
+	    var apiURL="";
+	    var apiJSON=j2API.getAPIJson(name);
+		apiURL=(app.data.dev)?"/api/jsonws/invoke/" + name + ".json":"/api/jsonws/invoke";
+		apiURL=(apiJSON.type=="url")?apiJSON.url:apiURL;
+		apiURL=app.data.baseURL+apiURL;
+		console.log("api.js",apiURL);
+		var apiParams=apiJSON.params;
         apiParams.p_auth=app.data.p_auth;
         for(var item in params){
             apiParams[item]=params[item];
-           }
-
-        Framework7.request.get(apiURL,apiParams, function (data) {
+        }
+		urlAndParams.url=apiURL;
+		urlAndParams.params=apiParams;
+		return urlAndParams;
+	},
+    postAPI: function (name, params,callback) {
+	    var uap=this.getAPIURLAndParams(name,params);
+         console.log(uap.params);
+       app.data.dev?
+        Framework7.request.get(uap.url,uap.params, function (data) {
+            var ret = JSON.parse(data);
+            callback(ret);
+        }):
+        Framework7.request.post(uap.url,uap.params, function (data) {
             var ret = JSON.parse(data);
             callback(ret);
         });
+		
     },
     getAPI: function (name,params,callback) {
-        Framework7.request.get('/api/jsonws/invoke/' + name + ".json", j2API.getAPIJson(name).params, function (data) {
+        Framework7.request.get(app.data.baseURL+'/api/jsonws/invoke/' + name + ".json", j2API.getAPIJson(name).params, function (data) {
             var ret = JSON.parse(data);
             var rows = ret.rows;
             callback(ret);
         });
     },
+	login:function(params,callback)
+	{ 
+	         
+		      this.postAPI("accountLogin",params,function(data){
+				  if (data.statusCode==200 && data.userId && data.userId != '') {
+					localStorage.setItem('userId', data.userId);
+					localStorage.setItem('p_auth', data.p_auth);
+					localStorage.setItem('account', params.account);
+					localStorage.setItem('password', params.password);
+					localStorage.setItem('userName', data.userName);
+					app.data.p_auth = data.p_auth;
+					app.data.userId = data.userId;
+					app.data.account = data.account;
+					app.data.password = data.password;
+					app.data.userName = data.userName;
+					
+					//need view the page again
+					    callback(true);
+				  } else {
+						callback(false);
+				  }
+				});
+	}
 }
 function J2API() {
 
--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,5 @@
 // Dom7
 var $$ = Dom7;
-
 // Framework7 App main instance
 var app = new Framework7({
   root: '#app', // App root element
@@ -12,22 +11,24 @@ var app = new Framework7({
   // App root data
   data: function () {
     return {
-      //gavin https://www.j2eecn.com
-      baseURL: '',
+      baseURL: 'https://www.j2eecn.com',
       p_auth: '',
       companyId: 20155,
       groupId: 0,
       userId: '',
-      userName: '刘洪义',
+      userName: '',
       account: '',
       password: '',
+	  dev:true,
+	  version:'v1.0.1',
+	  webCtx:'',
+	  defaultCT:'application/x-www-form-urlencoded;charset=UTF-8',
       //UI
       homeView: '',
       taskView: '',
       myView: ''
     };
   },
-
   // App root methods
   methods: {
 
@@ -43,11 +44,6 @@ var app = new Framework7({
     pageBeforeIn: function (e, page) {
       // do something after page gets into the view
       console.log("app.js", "pageBeforeIn");
-      console.log("app.js", e.name);
-     if(e.name=="net-error" || e.name=="login" || e.name=="wl-add")
-      {
-        //app.toolbar.hide('.toolbar');
-      }
     },
     pageAfterIn: function (e, page) {
       // do something after page gets into the view
@@ -58,120 +54,6 @@ var app = new Framework7({
   // App routes
   routes: routes,
 });
-
-//set up request
-app.request.setup({
-  contentType: 'application/x-www-form-urlencoded;charset=UTF-8'
-});
-// Init/Create views
-app.toolbar.hide('.toolbar');
-
-var homeURL = "/home/";
-app.preloader.show();
-initSys();
-app.preloader.hide();
-
-app.data.homeView = app.views.create('#view-home', {
-  url: homeURL
-});
-
-if (homeURL == "/home/") {
-  app.data.taskView = app.views.create('#view-task', {
-    url: '/task/'
-  });
-  app.data.myView = app.views.create('#view-my', {
-    url: '/my/'
-  });
-}
-
-
-//----------------------------------------------
-function initSys() {
-  if (!navigator.onLine) {
-    homeURL = "/mics/net-err/";
-    return;
-  }
-  var account = localStorage.getItem('account');
-  var password = localStorage.getItem('password');
-  if (account == null || account == '' || password == null || password == '') {
-    homeURL = "/login/";
-    return;
-  }
-  var params = {
-    companyId: app.data.companyId,
-    account: account,
-    password: password
-  };
-  Framework7.request({
-    method: "post",
-    contentType: "application/x-www-form-urlencoded;charset=UTF-8",
-    url: "https://www.j2eecn.com/j2-api/login/account_login",
-    async: false,
-    data: params,
-    success: function (data, status, xhr) {
-      var res = JSON.parse(data);
-      if (res.uesrId && res.uesrId != '') {
-        localStorage.setItem('userId', res.uesrId);
-        localStorage.setItem('p_auth', res.p_auth);
-        localStorage.setItem('account', account);
-        localStorage.setItem('password', password);
-        app.data.p_auth = res.p_auth;
-        app.data.userId = res.uesrId;
-        app.data.account = account;
-        app.data.password = password;
-        //to do need relogin
-        homeURL = "/home/";
-      } else {//to login
-        homeURL = "/login/";
-      }
-    },
-    error: function (xhr, status) {
-      homeURL = "/login/";
-      return;
-    },
-    statusCode: {
-      404: function (xhr) {
-      }
-    }
-  });
-}
-
-//------------------------------------------------------
-function getSession() {
-  var account = localStorage.getItem('account');
-  var password = localStorage.getItem('password');
-  if (account == null || account == '' || password == null || password == '') {
-    console.log("no account in localStorage");
-    return;
-  }
-  var params = {
-    companyId: app.data.companyId,
-    account: account,
-    password: password
-  };
-
-  j2API.postAPI("accountLogin", params, function (data) {
-    if (data.statusCode == 200 && data.userId && data.userId != '') {
-      localStorage.setItem('userId', data.userId);
-      localStorage.setItem('p_auth', data.p_auth);
-      localStorage.setItem('account', account);
-      localStorage.setItem('password', password);
-      app.data.p_auth = data.p_auth;
-      app.data.userId = data.userId;
-      app.data.account = account;
-      app.data.password = password;
-      console.log("app.js", "accountLogin ok");
-    } else {
-      console.log("app.js", "accountLogin error");
-    }
-  });
-}
-//----------------------------------
-
-var sessionKeeper = setInterval(getSession, 10000);
-
-//clearInterval(myRoll);
-
 
 
 
